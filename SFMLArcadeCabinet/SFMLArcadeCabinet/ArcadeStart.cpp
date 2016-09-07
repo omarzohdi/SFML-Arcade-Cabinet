877a16dc39ec8b6590c504e953363eb35c19835f
Omar Zohdi <omarzohdi@gmail.com>
2016-09-07 02:13:24 +0200

    File clean-up
    Removed unnecessary files.

--- SFMLArcadeCabinet/SFMLArcadeCabinet/ArcadeStart.cpp
+++ SFMLArcadeCabinet/SFMLArcadeCabinet/ArcadeStart.cpp
@@ -2,33 +2,6 @@
 #include <time.h>
 #include "ArcadeManager.h"
 
-/*
-int main__(void)
-{	
-	ArcadeManager Manager;
-
-	sf::Clock clock;
-	sf::RenderWindow window(sf::VideoMode(320, 480), "Arcade Cabinet");
-
-	Manager.Start();
-	while (window.isOpen())
-	{
-		sf::Event e;
-		while (window.pollEvent(e))
-		{
-			if (e.type == sf::Event::Closed)
-				window.close();
-			Manager.Events(e);
-		}
-
-		Manager.Process(clock, window);
-	}
-
-	Manager.Stop();
-
-	return 0;
-}
-*/
 int main(void)
 {
 	ArcadeManager manager;
--- SFMLArcadeCabinet/SFMLArcadeCabinet/cScreen.cpp
+++
@@ -1,1 +0,0 @@
-#include "cScreen.h"
